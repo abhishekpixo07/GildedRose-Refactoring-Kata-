786eacab21c86b48e99466f7596070a1339dba55
abhishekpixo07 <151621651+abhishekpixo07@users.noreply.github.com>
2023-12-07 15:26:45 +0530

    Update gilded_rose.rb

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,13 +1,70 @@
 class GildedRose
-  def initialize(items)
-    @items = items
+  def self.update_quality(items)
+    items.each do |item|
+      update_item_quality(item)
+    end
   end
 
-  # Delegates the update responsibility to each item
-  def update_quality
-    @items.each do |item|
-      item.update_quality
+  private
+
+  def self.update_item_quality(item)
+    case item.name
+    when "Aged Brie"
+      update_aged_brie(item)
+    when "Backstage passes to a TAFKAL80ETC concert"
+      update_backstage_passes(item)
+    when "Sulfuras, Hand of Ragnaros"
+      # Sulfuras does not change, nothing to do
+    when "Conjured"
+      update_conjured(item)
+    else
+      update_normal_item(item)
     end
+  end
+
+  def self.update_aged_brie(item)
+    increase_quality(item)
+    decrease_sell_in(item)
+    increase_quality(item) if item.sell_in.negative?
+  end
+
+  def self.update_backstage_passes(item)
+    increase_quality(item)
+    decrease_sell_in(item)
+
+    if item.sell_in < 10
+      increase_quality(item)
+    end
+
+    if item.sell_in < 5
+      increase_quality(item)
+    end
+
+    item.quality = 0 if item.sell_in.negative?
+  end
+
+  def self.update_conjured(item)
+    decrease_quality(item, 2)
+    decrease_sell_in(item)
+  end
+
+  def self.update_normal_item(item)
+    decrease_quality(item)
+    decrease_sell_in(item)
+    decrease_quality(item, 1) if item.sell_in.negative?
+  end
+
+  def self.decrease_quality(item, amount = 1)
+    item.quality -= amount if item.quality.positive?
+  end
+
+  def self.increase_quality(item)
+    item.quality += 1 if item.quality < 50
+  end
+
+  def self.decrease_sell_in(item)
+    item.sell_in -= 1 unless item.name == "Sulfuras, Hand of Ragnaros"
+    item.sell_in = 0 if item.name == "Aged Brie" && item.sell_in.negative? 
   end
 end
 
@@ -20,58 +77,7 @@ class Item
     @quality = quality
   end
 
-  def update_quality
-    if normal_item?
-      if expired?
-        decrease_quality(2)
-      else
-        decrease_quality
-      end
-    else
-      increase_quality
-      decrease_sell_in
-    end
-  end
-  
-
-  def to_s
+  def to_s()
     "#{@name}, #{@sell_in}, #{@quality}"
-  end
-
-  private
-
-  def normal_item?
-    @name != "Aged Brie" && @name != "Backstage passes to a TAFKAL80ETC concert" && @name != "Sulfuras, Hand of Ragnaros"
-  end
-  
-
-  def decrease_quality
-    @quality -= 1 if @quality.positive?
-  end
-
-  def increase_quality
-    @quality += 1 if @quality < 50
-  end
-
-  def decrease_sell_in
-    @sell_in -= 1 unless legendary_item?
-  
-    @sell_in = 0 if @sell_in.negative?  # Ensure minimum value is 0
-  
-    if expired?
-      normal_item? ? decrease_quality : increase_quality
-    end
-  end
-
-  def legendary_item?
-    @name == "Sulfuras, Hand of Ragnaros, Aged Brie"
-  end
-
-  def expired?
-    @sell_in < 0
-  end
-
-  def decrease_quality(amount = 1)
-    @quality -= amount if @quality.positive?
   end
 end
